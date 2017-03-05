e5d5c80cddbbfa628bf874811d1389434a44e1f3
Gamefreak <hagen.knobel@web.de>
2017-03-05 17:01:14 +0100

    Forgot to change debug settings

--- lua/autorun/percent.lua
+++ lua/autorun/percent.lua
@@ -392,7 +392,6 @@ if SERVER then
     ply:SetNWBool("CanSpawnVoteBeacon", false)
   end
 
-  --hook.Add("TTTPlayerSpeed", "TTTVoteBeacon", TTTPercent.AdjustSpeed)
   concommand.Add("ttt_resetallpercentages", TTTPercent.ResetPercentforEveryOne)
   concommand.Add("ttt_resetpercentage",TTTPercent.ResetPercentforOnePlayer, AutoCompletePercent)
   hook.Add("PlayerSay","TTTPercent", TTTPercent.GetPercentMessage)
@@ -454,9 +453,7 @@ elseif CLIENT then
       DComboBox:SetPos(100, frame:GetTall() / 2 - 10)
       DComboBox:SetValue( "Spieler" )
       for k,v in pairs(player.GetAll()) do
-        if !v:IsBot() 
-          --and v != LocalPlayer() 
-          and !v:GetDetective() then
+        if !v:IsBot() and v != LocalPlayer() and !v:GetDetective() then
           DComboBox:AddChoice(v:Nick(), v:SteamID())
         end
       end
@@ -468,7 +465,7 @@ elseif CLIENT then
       Slider:SetPos(frame:GetWide() - 250, frame:GetTall() / 2-50)
       Slider:SetText( "Prozent" )
       Slider:SetMin( 1 )
-      Slider:SetMax( 100 )
+      Slider:SetMax( 34 )
       Slider:SetDecimals( 0 )
       Slider:SetValue( 34 )
       local DButton2 = vgui.Create("DButton",frame)
