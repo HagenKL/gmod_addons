f7e8a75d89c2a077bfb822672740d1ed973838f0
Gamefreak <hagen.knobel@web.de>
2017-03-05 16:58:54 +0100

    Forgot

--- gamemodes/terrortown/entities/entities/ttt_votebeacon/shared.lua
+++ gamemodes/terrortown/entities/entities/ttt_votebeacon/shared.lua
@@ -1,18 +1,20 @@
 if SERVER then
   AddCSLuaFile()
-  resource.AddFile("models/radio_reference.mdl")
-  resource.AddFile("materials/models/props/radio.vmt")
+  resource.AddFile("models/frenchie/bulkytotem.mdl")
+  resource.AddFile("materials/frenchie/bulkytotem/ed3555af.vmt")
+  resource.AddFile("materials/frenchie/bulkytotem/a4c3dbeb.vmt")
+  resource.AddFile("materials/frenchie/bulkytotem/6348b211.vmt")
 end
 
 ENT.Type = "anim"
 ENT.Base = "base_anim"
-ENT.Model = Model("models/radio_reference.mdl")
+ENT.Model = Model("models/frenchie/bulkytotem.mdl")
 ENT.CanUseKey = true
 ENT.CanPickup = true
 
 function ENT:Initialize()
   self:SetModel(self.Model)
-
+  self:SetModelScale(0.1,0)
   if SERVER then
     self:PhysicsInit(SOLID_VPHYSICS)
   end
@@ -39,15 +41,18 @@ end
 function ENT:UseOverride(activator)
   if IsValid(activator) and activator:IsTerror() and self:GetOwner() == activator then
     activator:SetNWBool("CanSpawnVoteBeacon",true)
+    activator:SetNWBool("PlacedBeacon", false)
     activator:SetNWInt("VoteBeaconHealth",self:Health())
-    net.Start("TTTVoteBeaconPickUp")
-    net.Send(activator)
+      net.Start("TTTPercentBeacon")
+      net.WriteFloat(4)
+      net.Send(activator)
     self:Remove()
   end
 end
 
 local zapsound = Sound("npc/assassin/ball_zap1.wav")
 function ENT:OnTakeDamage(dmginfo)
+  if GetRoundState() != ROUND_ACTIVE then return end
   if dmginfo:GetInflictor() == self:GetOwner() or dmginfo:GetAttacker() == self:GetOwner() then return end
   self:TakePhysicsDamage(dmginfo)
 
@@ -61,16 +66,18 @@ function ENT:OnTakeDamage(dmginfo)
       net.Broadcast()
     end
 
+    if SERVER and self:GetOwner():IsValid() and dmginfo:GetAttacker():IsValid() and dmginfo:GetAttacker():IsPlayer() then
+      net.Start("TTTPercentBeacon")
+      net.WriteFloat(5)
+      net.WriteEntity(self:GetOwner())
+      net.WriteEntity(dmginfo:GetAttacker())
+      net.Send(self:GetOwner())
+    end
+
     local effect = EffectData()
     effect:SetOrigin(self:GetPos())
     util.Effect("cball_explode", effect)
     sound.Play(zapsound, self:GetPos())
-
-    if IsValid(self:GetOwner()) then
-      net.Start("TTTPercentBeacon")
-      net.WriteFloat(0)
-      net.Send(self:GetOwner())
-    end
   end
 end
 
