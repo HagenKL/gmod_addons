59241e5e7c2f28ba1e2aeff629506ca86ea114ff
Gamefreak <hagen.knobel@web.de>
2017-04-16 13:22:05 +0200

    Completely Rewamped Mirror Fate

--- gamemodes/terrortown/entities/weapons/weapon_ttt_mirrorfate/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_mirrorfate/shared.lua
@@ -2,22 +2,7 @@ if SERVER then
   AddCSLuaFile( "shared.lua" )
   resource.AddFile( "materials/vgui/ttt/icon_timer.vmt" )
   resource.AddWorkshop("611873052")
-
-  local PLAYER = FindMetaTable("Player")
-  util.AddNetworkString( "ColoredMessage" )
-  function BroadcastMsg(...)
-    local args = {...}
-    net.Start("ColoredMessage")
-    net.WriteTable(args)
-    net.Broadcast()
-  end
-
-  function PLAYER:PlayerMsg(...)
-    local args = {...}
-    net.Start("ColoredMessage")
-    net.WriteTable(args)
-    net.Send(self)
-  end
+  util.AddNetworkString("MFMessage")
 end
 
 if CLIENT then
@@ -30,11 +15,6 @@ if CLIENT then
     name = "Mirror Fate",
     desc = "If you get killed, your assassin will too! \nIf your assassin has this item too,\nit will have no effect.\nLeft-/Right-Click to adjust the death.\nReload to Reset!"
   }
-  net.Receive("ColoredMessage",function(len)
-      local msg = net.ReadTable()
-      chat.AddText(unpack(msg))
-      chat.PlaySound()
-    end)
 end
 
 SWEP.ViewModel = "models/weapons/v_watch.mdl"
@@ -72,91 +52,105 @@ if SERVER then
       buyer.fatetimemode = 30
     end
   end
-  function SWEP:SecondaryAttack()
-    local ply = self.Owner
-    ply.fatetimemode = ply.fatetimemode + 10
-    if ply.fatetimemode >= 100 then
-      ply.fatetimemode = 30
-    end
-    ply:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"The Fate will now take " .. ply.fatetimemode .. " seconds!")
-  end
   function SWEP:PrimaryAttack()
     local ply = self.Owner
     ply.fatemode = ply.fatemode + 1
     if ply.fatemode >= 4 then
       ply.fatemode = 1
     end
-    if ply.fatemode == 1 then
-      ply:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"Your killer will die on a heart-attack!")
-    elseif ply.fatemode == 2 then
-      ply:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"Your killer will explode!")
-    elseif ply.fatemode == 3 then
-      ply:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"Your killer will burn in Hell!")
+    local mode = ply.fatemode
+    if mode == 1 then
+      ply.fatemode = 30
+    elseif mode == 2 then
+      ply.fatemode = 40
+    elseif mode == 3 then
+      ply.fatemode = 50
     end
+    net.Start("MFMessage")
+    net.WriteInt(ply.fatemode, 8)
+    net.Send(ply)
   end
-  function KillTheKillerMirrorfate( victim, killer, damageinfo )
-    if IsValid(killer) and IsValid(victim) then
-      if !killer.DyeOnFate and victim:HasWeapon("weapon_ttt_mirrorfate") and !killer:HasWeapon("weapon_ttt_mirrorfate") then
-        killer.DyeOnFate = true
-        TTTMirrorfateKillHim(victim, killer)
-      end
-    end
-  end
-  function TTTMirrorfateKillHim(victim, killer)
+
+  function KillTheKillerMirrorfate(victim, killer)
     timer.Create( "MirrorFatekill" .. killer:EntIndex(), victim.fatetimemode or 30 , 1, function()
-        if IsValid(killer) then
-          if killer:IsTerror() and killer.DyeOnFate then
-            if victim.fatemode == 1 then
-              local dmginfo = DamageInfo()
-              dmginfo:SetDamage(10000)
-              dmginfo:SetAttacker(victim)
-              dmginfo:SetDamageType(DMG_GENERIC)
-              killer:TakeDamageInfo(dmginfo)
-            elseif victim.fatemode == 2 then
-              local effectdata = EffectData()
-              killer:EmitSound( Sound ("ambient/explosions/explode_4.wav") )
-              util.BlastDamage( victim, victim, killer:GetPos() , 200 , 1000 )
-              effectdata:SetStart( killer:GetPos() + Vector(0,0,10) )
-              effectdata:SetOrigin( killer:GetPos() + Vector(0,0,10) )
-              effectdata:SetScale( 1 )
-              util.Effect( "HelicopterMegaBomb", effectdata )
-            elseif victim.fatemode == 3 then
-              local dmg = DamageInfo()
-              dmg:SetDamage(5)
-              dmg:SetAttacker(victim)
-              dmg:SetDamageType(DMG_BURN)
-              killer:EmitSound("gamefreak/evillaugh.mp3")
-              timer.Create("BurnInHellMirrorfate" .. killer:EntIndex(), 0.25, 0, function()
-                  if killer:Alive() and killer:IsTerror() and IsValid(killer) then
-                    killer:TakeDamageInfo(dmg)
-                    killer:Ignite(0.2)
-                  elseif IsValid(killer) and !killer:IsTerror() then
-                    timer.Remove("BurnInHellMirrorfate" .. killer:EntIndex())
-                  end
-                end )
-            else
-              local dmginfo = DamageInfo()
-              dmginfo:SetDamage(10000)
-              dmginfo:SetAttacker(victim)
-              dmginfo:SetDamageType(DMG_GENERIC)
-              killer:TakeDamageInfo(dmginfo)
-            end
-            killer:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"You have experienced the " ,Color(255,0,0) ,"fate " ,Color(250,250,250) ,"your victim choose." )
-            victim:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"Your killer has shared your " ,Color(255,0,0), "fate." )
-          elseif IsValid(victim) and (!IsValid(killer) or !killer:IsTerror()) then
-            victim:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"Your killer is already dead!")
-          end
+      if IsValid(victim) and IsValid(killer) and killer:IsTerror() then
+        killer:TTTMirrorfate(victim)
+      elseif IsValid(victim) and (!IsValid(killer) or !killer:IsTerror()) then
+        victim:PlayerMsg("Mirror Fate: ", Color(250,250,250) ,"Your killer is already dead!")
+      end
+    end)
+  end
+
+  local function Mirrorfate( victim, killer, damageinfo )
+    if IsValid(killer) and IsValid(victim) and victim:HasWeapon("weapon_ttt_mirrorfate") and !killer:HasWeapon("weapon_ttt_mirrorfate") then
+      KillTheKillerMirrorfate(victim, killer)
+    end
+    if IsValid(victim) and timer.Exists("MirrorFatekill" .. victim:EntIndex()) then
+      timer.Remove("MirrorFatekill" .. victim:EntIndex())
+    end
+  end
+
+  hook.Add( "DoPlayerDeath" , "MirrorfateKillhim" , Mirrorfate )
+
+  local function MFHeartAttack(victim, killer)
+    local dmginfo = DamageInfo()
+    dmginfo:SetDamage(10000)
+    dmginfo:SetAttacker(victim)
+    dmginfo:SetDamageType(DMG_GENERIC)
+    killer:TakeDamageInfo(dmginfo)
+  end
+
+  local function MFBurn(victim, killer)
+    local dmg = DamageInfo()
+    dmg:SetDamage(5)
+    dmg:SetAttacker(victim)
+    dmg:SetDamageType(DMG_BURN)
+    killer:EmitSound("gamefreak/evillaugh.mp3")
+    timer.Create("BurnInHellMirrorfate" .. killer:EntIndex(), 0.25, 0, function()
+        if killer:Alive() and killer:IsTerror() and IsValid(killer) then
+          killer:TakeDamageInfo(dmg)
+          killer:Ignite(0.2)
+        elseif IsValid(killer) and !killer:IsTerror() then
+          timer.Remove("BurnInHellMirrorfate" .. killer:EntIndex())
         end
       end )
   end
-  hook.Add( "DoPlayerDeath" , "MirrorfateKillhim" , KillTheKillerMirrorfate )
+
+  local function MFExplode(victim, killer)
+    local effectdata = EffectData()
+    killer:EmitSound( Sound ("ambient/explosions/explode_4.wav") )
+    util.BlastDamage( victim, victim, killer:GetPos() , 200 , 1000 )
+    effectdata:SetStart( killer:GetPos() + Vector(0,0,10) )
+    effectdata:SetOrigin( killer:GetPos() + Vector(0,0,10) )
+    effectdata:SetScale( 1 )
+    util.Effect( "HelicopterMegaBomb", effectdata )
+  end
+
+  local plymeta = FindMetaTable("Player")
+
+  function plymeta:TTTMirrorfate(victim)
+    if victim.fatemode == 1 then
+      MFHeartAttack(victim, self)
+    elseif victim.fatemode == 2 then
+      MFBurn(victim, self)
+    elseif victim.fatemode == 3 then
+      MFExplode(victim, self)
+    else
+      MFHeartAttack(victim, self)
+    end
+    net.Start("MFMessage")
+    net.WriteInt(5,8)
+    net.Send(self)
+    net.Start("MFMessage")
+    net.WriteInt(10,8)
+    net.Send(victim)
+  end
 
   local function ResetMirrorFate(ply)
     timer.Remove("MirrorFatekill" .. ply:EntIndex())
     timer.Remove("BurnInHellMirrorfate" .. ply:EntIndex())
     ply.fatemode = 1
     ply.fatetimemode = 30
-    ply.DyeOnFate = false
   end
 
   hook.Add("PlayerSpawn", "ResetMirrorFate", function(ply)
@@ -166,5 +160,22 @@ if SERVER then
       for key,ply in pairs(player.GetAll()) do
         ResetMirrorFate(ply)
       end
-    end)
+  end)
+elseif CLIENT then
+  local function MFMessage()
+    local mode = net.ReadInt(8)
+    if mode == 1 then
+      chat.AddText("Mirror Fate: ", Color(250,250,250) ,"Your killer will die on a heart-attack, standart 30 seconds!")
+    elseif mode == 2 then
+      chat.AddText("Mirror Fate: ", Color(250,250,250) ,"Your killer will burn in Hell, but the style takes 10 more seconds!")
+    elseif mode == 3 then
+      chat.AddText("Mirror Fate: ", Color(250,250,250) ,"Your killer will explode, but it takes 20 more seconds than normal!")
+    elseif mode == 5 then
+      chat.AddText("Mirror Fate: ", Color(250,250,250) ,"You have experienced the " ,Color(255,0,0) ,"fate " ,Color(250,250,250) ,"your victim chose." )
+    elseif mode == 10 then
+      chat.AddText("Mirror Fate: ", Color(250,250,250) ,"Your killer has shared your " ,Color(255,0,0), "fate." )
+    end
+    chat.PlaySound()
+  end
+  net.Receive("MFMessage", MFMessage)
 end
