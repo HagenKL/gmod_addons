4c404aabe7c7ad21ffba16d77bd4cf8be458fd5d
Hagen <hagen.knobel@web.de>
2017-06-17 17:34:28 +0200

    Fixed Gravity

--- lua/randomat/events/moongravity.lua
+++ lua/randomat/events/moongravity.lua
@@ -6,11 +6,21 @@ function EVENT:Begin()
 	for _, ply in pairs(self:GetPlayers()) do
 		ply:SetGravity(0.1)
 	end
+	self:Timer()
+end
+
+function EVENT:Timer()
+	timer.Create("RandomatGravity", 1,0, function()
+		for _, ply in pairs(self:GetPlayers()) do
+			ply:SetGravity(0.1)
+		end
+		self:Timer()
+	end)
 end
 
 function EVENT:End()
 	self:CleanUpHooks()
-
+	timer.Remove("RandomatGravity")
 	for _, ply in pairs(player.GetAll()) do
 		ply:SetGravity(1)
 	end
