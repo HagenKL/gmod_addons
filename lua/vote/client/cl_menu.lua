3ac4626a53e8a4b680900c25c007d3fe0e495110
Hagen <hagen.knobel@web.de>
2017-09-07 00:04:11 +0200

    Fixed bugs

--- lua/vote/client/cl_menu.lua
+++ lua/vote/client/cl_menu.lua
@@ -43,7 +43,7 @@ local function OpenVoteMenu()
     end
   end
   ListView.DoDoubleClick = function(List, lineID,line)
-    if LocalPlayer():IsTerror() and GetRoundState() == ROUND_ACTIVE or LocalPlayer():GetNWInt("UsedVotes", 0) <= 0 then
+    if LocalPlayer():IsTerror() and GetRoundState() == ROUND_ACTIVE and LocalPlayer():GetNWInt("UsedVotes", 0) <= 0 then
       local nick,steamid = line:GetColumnText(1), line:GetColumnText(2)
       if isstring(steamid) and steamid != "NULL" and steamid != "BOT" then
         local ply = player.GetBySteamID(steamid)
